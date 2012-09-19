78b636fa536a9c1e772150a1926a608cedcd1af4
Luke Harris <lukehharris@gmail.com>
2012-09-18 20:08:17 -0400

    django2

--- ventureprime/views.py
+++ ventureprime/views.py
@@ -1,5 +1,13 @@
+from django.template.loader import get_template
+from django.template import Context
 from django.http import HttpResponse
+import datetime
 
 def homepage(request):
 	return HttpResponse("Welcome to VenturePrime")
 
+def current_datetime(request):
+    now = datetime.datetime.now()
+    t = get_template('current_datetime.html')
+    html = t.render(Context({'current_date': now}))
+    return HttpResponse(html)
