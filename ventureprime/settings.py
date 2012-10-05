34c65560c156b851842e233d9309c6faa97f0d54
Luke Harris <lukehharris@gmail.com>
2012-10-05 16:18:12 -0400

    fix email logins again

--- ventureprime/settings.py
+++ ventureprime/settings.py
@@ -2,7 +2,7 @@
 import os
 import socket
 
-#Turn debug off in production, keep in on in dev
+#Turn debug off in production, keep it on in dev
 if socket.gethostname() == 'JohnGalt':
     DEBUG = True
 else:
@@ -74,7 +74,8 @@ STATICFILES_DIRS = (
     # Put strings here, like "/home/html/static" or "C:/www/django/static".
     # Always use forward slashes, even on Windows.
     # Don't forget to use absolute paths, not relative paths.
-    os.path.join(os.path.dirname(__file__), 'static').replace('\\','/'),
+    
+    #os.path.join(os.path.dirname(__file__), 'static').replace('\\','/'),
 )
 
 # List of finder classes that know how to find static files in
